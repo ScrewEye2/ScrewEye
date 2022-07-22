b78d95e4b831a67bc55a278c29e7cf7e2eb5aa83
vova <neliverlist@gmail.com>
2022-07-23 01:56:27 +0600

    2

---
+++ script.js
@@ -0,0 +1,11 @@
+const linkBtn = document.querySelector(".Home")
+
+
+
+linkBtn.addEventListener("click", function() {
+    const home = document.querySelector('.content__home').style.display = 'block'
+})
+
+
+const menu = document.querySelector(".menu")
+console.log(menu)
